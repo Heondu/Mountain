a04ef19330d8b333c7a130a80ef190c5153e476b
Heondu <1217pgy@naver.com>
2022-03-13 22:26:21 +0900

    Changelog - 천공 섬 제작 - NPC 추가 및 Yarn 연결

---
+++ Assets/Scripts/YarnInteractable.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using Yarn.Unity;
+
+public class YarnInteractable : MonoBehaviour {
+
+    [SerializeField] private string conversationStartNode;
+
+    private DialogueRunner dialogueRunner;
+    private bool interactable = true;
+    private bool isCurrentConversation = false;
+
+    public void Start()
+    {
+        dialogueRunner = FindObjectOfType<DialogueRunner>();
+        dialogueRunner.onDialogueComplete.AddListener(EndConversation);
+    }
+
+    public void OnMouseDown()
+    {
+        if (interactable && !dialogueRunner.IsDialogueRunning)
+            StartConversation();
+    }
+
+    private void StartConversation()
+    {
+        Debug.Log($"Started conversation with {name}.");
+        isCurrentConversation = true;
+        dialogueRunner.StartDialogue(conversationStartNode);
+    }
+
+    private void EndConversation()
+    {
+        if (isCurrentConversation)
+        {
+            isCurrentConversation = false;
+            Debug.Log($"Started conversation with {name}.");
+        }
+    }
+
+    [YarnCommand("disable")]
+    public void DisableConversation()
+    {
+        interactable = false;
+    }
+}
